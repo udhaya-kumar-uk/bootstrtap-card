5e4ac97da596f9dafa5493e136c348c8700a148a
udhaya-kumar-uk <udhayakumar1451@gmail.com>
2023-04-11 17:28:40 +0530

    first react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Card from  "./Componentpages/Card.js" ;   // './Componentpages/Card.js'
+               
+              
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+       <section class="pricing py-5">
+       <div class="container">
+       <div class="row">
+        <Card price="10" user="Single User" storage="10GB" />
+        <Card  price="20" user="Double Users"  storage="20GB"/>
+        <Card  price="30" user="Triple Users"  storage="30GB"/>
+        <Card  price="40"  user="Four Users"  storage="40GB"/>
+        <Card  price="50" user="Five Users" storage="50GB"/>
+        <Card price="1000" user="Multiple Users" storage="UNLIMITED"/>
+      </div>
+      </div>
+      </section>
     </div>
+      
   );
 }
 
